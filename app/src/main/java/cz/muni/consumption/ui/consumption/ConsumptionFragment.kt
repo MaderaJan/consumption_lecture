ff7c7c52dee29676e1cb7511d91289cba2fadd46
honzam <gmaderajan@gmail.com>
2023-02-05 10:43:00 +0100

    1. lecture - start

---
+++ app/src/main/java/cz/muni/consumption/ui/consumption/ConsumptionFragment.kt
@@ -0,0 +1,6 @@
+package cz.muni.consumption.ui.consumption
+
+import androidx.fragment.app.Fragment
+import cz.muni.consumption.R
+
+class ConsumptionFragment : Fragment(R.layout.fragment_consumption)
---
+++ app/src/main/java/cz/muni/consumption/ui/trends/TrendsFragment.kt
@@ -0,0 +1,7 @@
+package cz.muni.consumption.ui.trends
+
+import androidx.fragment.app.Fragment
+import cz.muni.consumption.R
+
+// TODO 3 Fragment
+class TrendsFragment : Fragment(R.layout.fragment_trends)

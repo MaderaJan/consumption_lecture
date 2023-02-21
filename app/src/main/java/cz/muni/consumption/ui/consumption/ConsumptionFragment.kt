6a27c84b34645dceff88183cf809b0ecebf4f53e
Jan Maděra <maderajan@email.cz>
2023-02-21 08:52:04 +0100

    2. lecture - end

--- app/src/main/java/cz/muni/consumption/ui/consumption/ConsumptionFragment.kt
+++ app/src/main/java/cz/muni/consumption/ui/consumption/ConsumptionFragment.kt
@@ -5,15 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import cz.muni.consumption.R
 import cz.muni.consumption.databinding.FragmentConsumptionBinding
 
 class ConsumptionFragment : Fragment() {
 
-    // TODO 8. ListFragment binding
     private lateinit var binding: FragmentConsumptionBinding
 
-    // TODO 9. onCreateView && Jiné vytvoření View pro Fragment jak pro Activitu
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentConsumptionBinding.inflate(layoutInflater, container, false)
         return binding.root
--- app/src/main/java/cz/muni/consumption/ui/trends/TrendsFragment.kt
+++ app/src/main/java/cz/muni/consumption/ui/trends/TrendsFragment.kt
@@ -1,15 +1,28 @@
 package cz.muni.consumption.ui.trends
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import cz.muni.consumption.R
+import cz.muni.consumption.databinding.FragmentTrendsBinding
+import java.util.*
 
-// TODO 11. (S) Vytvoření bidning pro trends fragment
-class TrendsFragment : Fragment(R.layout.fragment_trends)
+class TrendsFragment : Fragment() {
 
-// TODO 14. (S) napsat funkci, která
-// TODO - po klikud na tlačtíko -> button.setOnClickListener { }
-// TODO - veme text z TextView -> current_date_text_view
-// TODO - Získá aktuální čas
-// TODO - a spojí předchozí text se získaným časem
-// System.currentTimeMillis()
-// Calendar.getInstance()
+    private lateinit var binding: FragmentTrendsBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentTrendsBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        binding.tlacitko.setOnClickListener {
+            val c = Calendar.getInstance()
+            binding.currentDateTextView.text = "${System.currentTimeMillis()}, " + binding.currentDateTextView.text
+        }
+    }
+}

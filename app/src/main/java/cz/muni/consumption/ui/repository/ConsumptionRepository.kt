71d28a6de4d6dd1a9c6d3619be60e6055733e046
Jan Maděra <maderajan@email.cz>
2023-02-27 19:29:10 +0100

    3. lecture - end

--- app/src/main/java/cz/muni/consumption/ui/repository/ConsumptionRepository.kt
+++ app/src/main/java/cz/muni/consumption/ui/repository/ConsumptionRepository.kt
@@ -1,9 +1,25 @@
 package cz.muni.consumption.ui.repository
 
+import cz.muni.consumption.ui.data.ConsumptionType
 import cz.muni.consumption.ui.data.MeasuredConsumption
+import java.util.*
 
 class ConsumptionRepository {
 
-    // TODO 11.3 (S) Vytvoření mocku pro MeasuredConsumption ?hint: repeat, for, etc...
-    fun getAllMeasuredConsumption(): List<MeasuredConsumption> = emptyList()
+    fun getAllMeasuredConsumption(): List<MeasuredConsumption> {
+        val result = mutableListOf<MeasuredConsumption>()
+
+        repeat(100) {
+            result.add(
+                MeasuredConsumption(
+                    id = it.toLong(),
+                    consumption = it.toDouble() + Math.random(),
+                    measurementDate = Date(System.currentTimeMillis()),
+                    type = if (it % 2 == 0) ConsumptionType.GAS else ConsumptionType.ELECTRICITY,
+                )
+            )
+        }
+
+        return result
+    }
 }
--- app/src/main/java/cz/muni/consumption/ui/util/extensions.kt
+++ app/src/main/java/cz/muni/consumption/ui/util/extensions.kt
@@ -1,5 +1,4 @@
 package cz.muni.consumption.ui.util
 
-// TODO 4.1 Extension for Double type
 fun Double.oneDecimal(): String =
     String.format("%.1f", this)

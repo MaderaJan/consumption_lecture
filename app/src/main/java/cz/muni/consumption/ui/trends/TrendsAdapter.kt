89c3a5e3f4b12362c457083ddb2a1c3092ea676c
Jan Maděra <maderajan@email.cz>
2023-03-06 19:39:24 +0100

    4. lecture - end

--- app/src/main/java/cz/muni/consumption/ui/trends/TrendsAdapter.kt
+++ app/src/main/java/cz/muni/consumption/ui/trends/TrendsAdapter.kt
@@ -1,5 +1,45 @@
 package cz.muni.consumption.ui.trends
 
-// TODO Hint: formátoví čísel pomocí extension oneDecimal
-// TODO Hint: formátování datumu DateUtil.yearMonthFormat.format(item.date)
-// TODO Hint: Trend differ areItemsTheSame -> date
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import cz.muni.consumption.databinding.ItemTrendBinding
+import cz.muni.consumption.ui.data.TrendData
+import cz.muni.consumption.ui.util.DateUtil
+import cz.muni.consumption.ui.util.oneDecimal
+
+class TrendAdapter : ListAdapter<TrendData, TrendViewHolder>(TrendDiffUtil()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrendViewHolder =
+        TrendViewHolder(ItemTrendBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+
+    override fun onBindViewHolder(holder: TrendViewHolder, position: Int) {
+        val item = getItem(position)
+        holder.bind(item)
+    }
+}
+
+class TrendViewHolder(private val binding: ItemTrendBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(item: TrendData) {
+        binding.consumptionTextView.text = item.consumption.oneDecimal() + " ${item.type.getUnit()}"
+        binding.priceTextView.text = item.price.oneDecimal() + " Kč"
+        binding.dateTextView.text = DateUtil.yearMonthFormat.format(item.date)
+        binding.temperatureTextView.text = if (item.temperature != null) {
+            "${item.temperature.oneDecimal()} Celsius"
+        } else {
+            "- -"
+        }
+    }
+}
+
+class TrendDiffUtil : DiffUtil.ItemCallback<TrendData>() {
+
+    override fun areItemsTheSame(oldItem: TrendData, newItem: TrendData): Boolean =
+        oldItem.date.compareTo(newItem.date) == 0
+
+    override fun areContentsTheSame(oldItem: TrendData, newItem: TrendData): Boolean =
+        oldItem == newItem
+}
--- app/src/main/java/cz/muni/consumption/ui/trends/TrendsFragment.kt
+++ app/src/main/java/cz/muni/consumption/ui/trends/TrendsFragment.kt
@@ -5,16 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import cz.muni.consumption.databinding.FragmentTrendsBinding
+import cz.muni.consumption.ui.data.ConsumptionType
 import cz.muni.consumption.ui.repository.TrendRepository
 
-// TODO 2. (S) - Vytvoření Trend obrazovky s 2 horizontal recyclery viz prezentace
-
-// TODO Hint: Step by Step
-// TODO 1. Vytvořit layout s recycler views viz prezentace -> fragment_trends.xml
-// TODO 2. Vytvořit layout -> item_trend.xml
-// TODO 3. Vytvořit ListAdapter, ViewHolder, DiffUtil v -> TrendsAdapter.kt
-// TODO 4. Propojit adapter, recycler a data z TrendRepository.kt
 class TrendsFragment : Fragment() {
 
     private lateinit var binding: FragmentTrendsBinding
@@ -23,16 +18,30 @@ class TrendsFragment : Fragment() {
         TrendRepository()
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentTrendsBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 
     override fun onStart() {
         super.onStart()
 
-        // TODO Hint: check TrendRepository
-        // TODO Hint: Horizontal recycler -> LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
-        // TODO Hint: adapter.submitList(data) s použitím TrendRepository
+        binding.electricityRecycler.layoutManager =
+            LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        val electricityAdapter = TrendAdapter()
+        binding.electricityRecycler.adapter = electricityAdapter
+
+        binding.gasRecycler.layoutManager =
+            LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        val gasAdapter = TrendAdapter()
+        binding.gasRecycler.adapter = gasAdapter
+
+        electricityAdapter.submitList(trendRepository.getMockedTrends(ConsumptionType.ELECTRICITY))
+        gasAdapter.submitList(trendRepository.getMockedTrends(ConsumptionType.GAS))
     }
+
 }
